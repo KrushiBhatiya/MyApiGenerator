047fea0b22099eb57c84d2b5627389bdf84342b5
KrushiBhatiya <bhatiyakrushi.project@gmail.com>
2025-05-20 19:05:30 +0530

    email

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 var cors = require('cors')
-mongoose.connect('mongodb+srv://admin:admin@cluster0.w16vr.mongodb.net/generateapi?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true, bufferCommands: false })
-.then(() => {
-  console.log("success");
-})
-.catch((error) => {
-  console.log(error.message);
-})
-// mongoose.connect('mongodb://localhost:27017/generateapi')
+// mongoose.connect('mongodb+srv://admin:admin@cluster0.w16vr.mongodb.net/generateapi?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true, bufferCommands: false })
 // .then(() => {
 //   console.log("success");
 // })
 // .catch((error) => {
 //   console.log(error.message);
 // })
+mongoose.connect('mongodb://localhost:27017/generateapi')
+.then(() => {
+  console.log("success");
+})
+.catch((error) => {
+  console.log(error.message);
+})
 mongoose.set('bufferTimeoutMS', 20000);
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api')
