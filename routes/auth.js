fea9c2d38db06ac5b2045b60e665868b805efcdb
KrushiBhatiya <bhatiyakrushi.project@gmail.com>
2025-05-20 19:26:50 +0530

    mid

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var TC = require('../middleware/tokenCheck')
 var AC = require('../controller/apiController')
 
-router.post('/signUp' , AC.authCreateData)
-router.post('/login' , AC.authDataCheck)
+router.post('/signUp' , TC.checkToken , AC.authCreateData)
+router.post('/login' , TC.checkToken  , AC.authDataCheck)
 
 module.exports = router;
